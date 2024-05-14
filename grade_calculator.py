7997be8f8989d096895010ad5d214801cbdcd0e9
Chris Laliberte <chris.laliberte@BCFEDU.local>
2024-05-14 15:49:15 -0400

    grade_calaculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,12 +1,12 @@
 def calculate_average_grade(): #This is a script that has a user input their name and grades in three subjects, finds their grade average, then outputs a name and averaged grade
     # Prompt the user for their name and store it in the student_name variable
-    student_name = input()
+    student_name = input("Student Name_")
 
     # Prompt the user for their scores in Math, Science, and English
     # Store the scores in the respective variables: math_score, science_score, english_score
-    math_score = input()
-    science_score = input()
-    english_score = input()
+    math_score = input("Math Score_")
+    science_score = input("Science Score_")
+    english_score = input("English Score_")
 
     # Calculate the average grade
     average_grade = (int(math_score)+int(science_score)+int(english_score))/3 #This line takes the user inputted scores, turns them to integers, sums them, then divides by 3
@@ -19,4 +19,4 @@ if __name__ == '__main__':
     student_name, average_grade = calculate_average_grade()
 
     # Print the student's name and their average grade
-    print(f"{student_name}",average_grade)
+    print(f"{student_name},","Grade =",average_grade)
